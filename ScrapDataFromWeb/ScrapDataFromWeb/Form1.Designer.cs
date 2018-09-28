7b12276a9152cb3157cc87d73e4827f6aa739f27
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-09-27 22:00:02 -0400

    Web Scrapping using C#
    On this tutorial you will learn how to web Scrapping using C# .NET.

---
+++ ScrapDataFromWeb/ScrapDataFromWeb/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ScrapDataFromWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnOpen = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.RichTextBox();
+            this.btnScrap = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnOpen
+            // 
+            this.BtnOpen.Location = new System.Drawing.Point(12, 380);
+            this.BtnOpen.Name = "BtnOpen";
+            this.BtnOpen.Size = new System.Drawing.Size(197, 58);
+            this.BtnOpen.TabIndex = 0;
+            this.BtnOpen.Text = "Open Browser";
+            this.BtnOpen.UseVisualStyleBackColor = true;
+            this.BtnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(12, 12);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(766, 348);
+            this.txtResults.TabIndex = 1;
+            this.txtResults.Text = "";
+            // 
+            // btnScrap
+            // 
+            this.btnScrap.Location = new System.Drawing.Point(239, 380);
+            this.btnScrap.Name = "btnScrap";
+            this.btnScrap.Size = new System.Drawing.Size(292, 58);
+            this.btnScrap.TabIndex = 2;
+            this.btnScrap.Text = "Scrap Data";
+            this.btnScrap.UseVisualStyleBackColor = true;
+            this.btnScrap.Click += new System.EventHandler(this.btnScrap_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(582, 380);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(196, 58);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnScrap);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.BtnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnOpen;
+        private System.Windows.Forms.RichTextBox txtResults;
+        private System.Windows.Forms.Button btnScrap;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ScrapDataFromWeb/ScrapDataFromWeb/Form1.cs
@@ -0,0 +1,95 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ScrapDataFromWeb
+{
+    public partial class Form1 : Form
+    {
+        public IWebDriver driver;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void Open_Browser()
+        {
+            
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+
+            //var options = new ChromeOptions();
+            //options.AddArgument("headless");
+
+            driver = new ChromeDriver(driverService);
+
+
+            try
+            {
+                driver.Navigate().GoToUrl("https://money.cnn.com/data/hotstocks/index.html");
+            }
+            catch
+            {
+                throw;
+            }
+
+            
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            Open_Browser();
+        }
+
+
+        private void Find_Data()
+        {
+            IList<IWebElement> searchElements = driver.FindElements(By.TagName("tbody"));
+
+            foreach (IWebElement i in searchElements)
+            {
+
+                HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                var text = i.GetAttribute("innerHTML");
+                htmlDocument.LoadHtml(text);
+
+                var inputs = htmlDocument.DocumentNode.Descendants("tr").ToList();
+
+                foreach (var items in inputs)
+                {
+                    HtmlAgilityPack.HtmlDocument htmlDocument1 = new HtmlAgilityPack.HtmlDocument();
+                    htmlDocument1.LoadHtml(items.InnerHtml);
+
+                    var tds = htmlDocument1.DocumentNode.Descendants("td").ToList();
+
+                    if(tds.Count != 0)
+                        txtResults.AppendText(tds[0].InnerText + " " + tds[1].InnerText +" " + tds[2].InnerText + " " + tds[3].InnerText + "\t\r");
+                }
+
+                txtResults.AppendText("\t\r");
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if(driver!= null)
+                driver.Quit();
+        }
+
+        private void btnScrap_Click(object sender, EventArgs e)
+        {
+            Find_Data();
+        }
+    }
+}
